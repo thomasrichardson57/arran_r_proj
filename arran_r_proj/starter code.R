9d99e76aa40747da9a425f2d41a9da1fe2676e31
Thomas Richardson <thomasrichardson20031@gmail.com>
2025-12-03 09:57:04 +0000

    Update starter code.R

--- arran_r_proj/starter code.R
+++ arran_r_proj/starter code.R
@@ -1,27 +1,4 @@
-numbers <- c(1,2,3,4) # numeric vector
-species <- c('blue tit', 'chaffinch', 'nuthatch') #character vector
-flags <- c(T,F,T) #logical vector (e.g, for pres/abs)
 
-m <- matrix(1:9, nrow= 3, ncol = 3)
-m
-
-df <- data.frame(
-  location = c('site1','site2','site3'),
-  species.no = c(25,30,2),
-  forest = c(T,F,T)
-)
-df
-
-mylist <- list(
-  numbers <- c(1,2,3),
-  species = c('seal','fox'),
-  data = df
-)
-mylist
-
-species[2] #selects the second species in the vector, i.e. chaffinch
-
-m[3,2] #selects the third row and second column, i.e. 6
 
 
 
